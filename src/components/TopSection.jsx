eeb54cf8c453cd4a02e09afdeb5a0e0db8bf8351
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-04 10:09:48 +0200

    Add video style

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -3,7 +3,7 @@ import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 import backgroundVideo from '../assets/backgroundVideo.mp4'
 import GraButton from './button/GraButton';
 
-const TopSection = ({isMenuOn}) => {
+const TopSection = ({isMenu}) => {
     const [isVideoOn, setIsVideoOn] = useState(true);
     const videoRef = useRef();
 
@@ -19,11 +19,11 @@ const TopSection = ({isMenuOn}) => {
 
   return (
     <div className="flex text-center h-screen w-full justify-center items-end">
-        <video ref={videoRef} src={backgroundVideo} muted loop
+        <video ref={videoRef} src={backgroundVideo} muted loop autoPlay
         className='h-full w-full absolute -z-10 object-cover' />
         <div className={`flex flex-col justify-center items-center duration-500 w-full p-8 ${
             isVideoOn ? '' : 'bg-black/80'
-        } ${isMenuOn ? 'opacity-20' : 'opacity-100'}`}>
+        } ${isMenu ? 'opacity-20' : 'opacity-100'}`}>
             <h1 className='text-5xl lg:text-7xl'>Web 3.0</h1>
             <h1 className='mb-14 text-5xl lg:text-7xl'>
             The <span className='font-bold text-tBlue'>Future</span> Is Here</h1>
@@ -32,9 +32,9 @@ const TopSection = ({isMenuOn}) => {
                 <GraButton className='mx-12 w-40' name='Products' />
                 {
                     isVideoOn ? (
-                        <FaPauseCircle size={30} />
+                        <FaPauseCircle size={30} className='text-tBlue cursor-pointer duration-300 hover:scale-125' onClick={handlePause} />
                     ) : (
-                        <FaPlayCircle size={30} />
+                        <FaPlayCircle size={30} className='text-tBlue cursor-pointer duration-300 hover:scale-125 ' onClick={handlePlay} />
                     )
                 }
             </div>
