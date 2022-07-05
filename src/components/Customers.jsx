5ff8b03e760b98a590de44a6d9e9efde19965d39
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-05 11:01:44 +0200

    Add style for customers component

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -27,6 +27,33 @@ const Customers = () => {
             details: textSmall
         }
     ];
+    const companies = [
+        {
+            id: 1,
+            icon: <FaAmazon size={25} />,
+            title: 'Amazon'
+        },
+        {
+            id: 2,
+            icon: <FaTwitter size={25} />,
+            title: 'Twitter'
+        },
+        {
+            id: 3,
+            icon: <FaGoogle size={25} />,
+            title: 'Google'
+        },
+        {
+            id: 4,
+            icon: <FaFacebook size={25} />,
+            title: 'Facebook'
+        },
+        {
+            id: 5,
+            icon: <FaApple size={25} />,
+            title: 'Apple'
+        }
+    ]
 
   return (
     <PageSection name='Customers' title='Customers' subtitle={textInside}>
@@ -37,6 +64,16 @@ const Customers = () => {
                     <div key={d.id} className='rounded-lg p-4 text-black odd:bg-tBlue even:bg-tOrange text-center'>
                         <h1 className='mb-2 text-3xl font-bold'>{d.title}</h1>
                         <p>{d.details}</p>
+                    </div>
+                ))
+            }
+        </div>
+        <div className='flex justify-around items-center flex-col lg:flex-row gap-6'>
+            {
+                companies.map(c => (
+                    <div key={c.id} className='flex rounded-lg p-4 items-center'>
+                        {c.icon}
+                        <p className='ml-2'>{c.title}</p>
                     </div>
                 ))
             }
