40d0cc048486f5eb33f15b103e9fe6f1b033fa63
denisdiaconu <denisdiaconu117@yahoo.com>
2022-07-06 10:36:33 +0200

    Add style for footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,35 @@
 import React from 'react'
+import FooterCard from './FooterCard'
 
 const Footer = () => {
+    const links = [
+        {
+            id: 1,
+            link: 'Link 1',
+        },
+        {
+            id: 2,
+            link: 'Link 2',
+        },
+        {
+            id: 3,
+            link: 'Link 3',
+        },
+        {
+            id: 4,
+            link: 'Link 4',
+        },
+    ]
+
+
   return (
     <div className='border-white border-t'>
         <div className='mx-auto my-20 max-w-screen-xl'>
             <div className='grid text-center gap-12 grid-cols-2 lg:grid-cols-4'>
-                <div>
-                    <h1 className='font-bold mb-8 text-lg lg:text-3xl'>Company</h1>
-                    <ul className='mt-1'>
-                        <li>Subment 1</li>
-                        <li>Subment 2</li>
-                        <li>Subment 3</li>
-                        <li>Subment 4</li>
-                    </ul>
-                </div>
-                <div>
-                    <h1 className='font-bold mb-8 text-lg lg:text-3xl'>Developer</h1>
-                    <ul className='mt-1'>
-                        <li>Subment 1</li>
-                        <li>Subment 2</li>
-                        <li>Subment 3</li>
-                        <li>Subment 4</li>
-                    </ul>
-                </div>
-                <div>
-                    <h1 className='font-bold mb-8 text-lg lg:text-3xl'>Support</h1>
-                    <ul className='mt-1'>
-                        <li>Subment 1</li>
-                        <li>Subment 2</li>
-                        <li>Subment 3</li>
-                        <li>Subment 4</li>
-                    </ul>
-                </div>
-                <div>
-                    <h1 className='font-bold mb-8 text-xl lg:text-3xl'>Other</h1>
-                    <ul className='mt-1'>
-                        <li>Subment 1</li>
-                        <li>Subment 2</li>
-                        <li>Subment 3</li>
-                        <li>Subment 4</li>
-                    </ul>
-                </div>
+                <FooterCard title='Company' list={links} />
+                <FooterCard title='Developer' list={links} />
+                <FooterCard title='Support' list={links} />
+                <FooterCard title='Other' list={links} />
             </div>
         </div>
         <p className='text-center text-tBlue text-sm my-12 font-light'>© Copyright 2022 LAPAGE</p>
--- src/components/FooterCard.jsx
+++ src/components/FooterCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const FooterCard = () => {
+const FooterCard = ({title, list}) => {
   return (
-    <div>FooterCard</div>
+    <div>
+        <h1 className='font-bold mb-8 text-lg lg:text-3xl'>{title}</h1>
+        <ul className='mt-1'>
+        {
+        list.map(i => (
+            <li key={i.id}>{i.link}</li>
+        ))
+        }
+        </ul>
+    </div>
   )
 }
 
